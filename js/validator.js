a14572759d3be764a55caf90eb56a97f32a3a5d4
pimpoyo <pimpoyolega@gmail.com>
2015-12-01 13:30:27 +0100

    Fix response in form

--- js/validator.js
+++ js/validator.js
@@ -8,7 +8,7 @@ $("#form1").submit(function(event) {
       success: function(data) {
         result = jQuery.parseJSON(data);
         if (result.ok != undefined && result.ok == true) {
-          form.html('<h4 class="form-success">El formulario ha sido enviado con éxito</h4>');
+          form.html('<div class="form-success"><h4>El formulario ha sido enviado con éxito</h4><h4>Muchas gracias por ponerse en contacto conmigo</h4></div>');
         } else {
           if (result.name != undefined) {
             $("#name").addClass("form-error")
